81c366026a81bf7aaad0698b189b6395e6c196c0
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2026-02-03 20:31:57 +0545

    admin users routes added

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
+import { authorizedMiddleware } from "../middlewares/authorized.middleware";
+import { uploads } from "../middlewares/upload.middleware";
 
 let authController = new AuthController();
 const router = Router();
@@ -7,5 +9,7 @@ const router = Router();
 router.post("/register", authController.register)
 router.post("/login", authController.login)
 // add remaning routes like login, logout, etc.
+router.put("/update", authorizedMiddleware, uploads.single("profile"), authController.updateUser)
+router.get("/users/:id", authController.getUserById)
 
 export default router;
