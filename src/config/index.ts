026654927f7000ad605b0140595666f1907adf71
AayushShrestha6163 <aayushshrestha6163@gmail.com>
2026-02-14 21:23:42 +0545

    email req and pagination

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,3 +10,12 @@ export const MONGODB_URI: string =
 
 export const JWT_SECRET: string = 
     process.env.JWT_SECRET || 'default'
+
+ export const CLIENT_URL: string =
+    process.env.CLIENT_URL || 'http://localhost:3000';
+   
+export const EMAIL_USER: string =
+    process.env.EMAIL_USER || 'aayushshrestha6163@gmail.com'
+
+export const EMAIL_PASS: string =
+    process.env.EMAIL_PASS || 'password';
